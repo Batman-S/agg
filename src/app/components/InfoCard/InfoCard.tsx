08aa805e8209b78a46de5b0e97b877dffd914329
Stephen Batman <batmania@Stephens-MacBook-Air.local>
2023-09-13 19:07:35 +0900

    intial commit

---
+++ src/app/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+const InfoCard = () => {
+  return (
+    <div className="bg-slate-200 px-4 py-4">
+      <div>image</div>
+      <div>title</div>
+      <div>price</div>
+      <div>size</div>
+    </div>
+  );
+};
+
+export default InfoCard;
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+const Navbar = () => {
+  return (
+    <div className="bg-slate-500 text-white px-2 py-2 font-semibold text-lg">
+      Capiba
+    </div>
+  );
+};
+
+export default Navbar;
